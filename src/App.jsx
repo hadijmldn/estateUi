4ca14f3da3317620ee4d21eb957bd54cbbc6bfa3
Hadi Jamaludin <hadijmldn@gmail.com>
2024-03-09 19:03:37 +0700

    123

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import HomePage from "./pages/homePage/HomePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ListPage from "./pages/listPage/ListPage";
+import Layout from "./pages/layout/Layout";
+import SinglePage from "./pages/singlePage/SinglePage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/homePage/HomePage.jsx
@@ -0,0 +1,40 @@
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione
+            neque officia tempora itaque iure ipsa quia earum, impedit,
+            blanditiis dicta cumque ipsam similique, molestias et minima porro
+            doloremque numquam nihil.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
